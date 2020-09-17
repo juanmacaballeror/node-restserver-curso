1b6dac10315f5feaa93719c51892a0fbe95c1214
unknown <Administrador@3ZFCTT2.ineco.es>
2020-09-17 09:12:48 +0200

    cambio uri

--- server/config/config.js
+++ server/config/config.js
@@ -22,8 +22,8 @@ if (process.env.NODE_ENV === 'dev') {
 } else {
   //para saber esta url lo primero que hecho es una nueva conexion en mongoDb compass al dataBase 'cafe', despues he desconectado y al volver a conectar ya salia esta url.accordion
   //no encuentro en el curso como obtener la url como se indica en la clase 109
-  // urlDB = 'mongodb+srv://admin:We1KozYOR2Yf464r@cluster0.yhmml.mongodb.net/cafe?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-  urlDB = 'mongodb+srv://admin:We1KozYOR2Yf464r@cluster0.yhmml.mongodb.net/admin?authSource=admin&replicaSet=atlas-h9l7ss-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+  urlDB = 'mongodb+srv://admin:We1KozYOR2Yf464r@cluster0.yhmml.mongodb.net/cafe?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+  //urlDB = 'mongodb+srv://admin:We1KozYOR2Yf464r@cluster0.yhmml.mongodb.net/admin?authSource=admin&replicaSet=atlas-h9l7ss-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 }
 
 process.env.URLDB = urlDB;
